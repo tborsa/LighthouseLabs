e520f7a0e7c62a74e6110efb460d5fe368825071
travis <travis.borsa@gmail.com>
2019-04-08 14:11:00 -0700

    W2D1

--- lectures/Week2/Day1/lecture/code/whosThatPokemonexample.js
+++ lectures/Week2/Day1/lecture/code/whosThatPokemonexample.js
@@ -4,18 +4,17 @@ function whosThatPokemon(number, callback){
     https.get('https://pokeapi.co/api/v2/pokemon/'+number, function(response){
         if(response.statusCode!== 200){
             callback(new Error("Invalid Response."+ response.statusCode))
-        }
-
-        response.setEncoding("utf-8");
-        var body = "";
-        response.on("data", function(chunk){
-            //onsole.log(chunk+"\n");
-            body += chunk;
-        });
-        response.on("end", function(){
-            callback(null, JSON.parse(body));
-        })
-    
+        }else{
+            response.setEncoding("utf-8");
+            var body = "";
+            response.on("data", function(chunk){
+                //onsole.log(chunk+"\n");
+                body += chunk;
+            });
+            response.on("end", function(){
+                callback(null, JSON.parse(body));
+            })
+        }  
     })
 }
 var number = 25;
